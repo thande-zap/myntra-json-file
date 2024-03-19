055f141889e0c32e258186d254c0763b07064102
Devansh Batra <83891794+thande-zap@users.noreply.github.com>
2024-03-19 19:26:15 +0530

    Delete url.js

--- url.js
+++
@@ -1,7 +0,0 @@
-const url = [
-  'https://www.mmtcpamp.com/gold-silver-rate-today',
-  'https://www.gadgets360.com/finance/gold-rate-in-india',
-  'https://ibjarates.com/',
-  'https://economictimes.indiatimes.com/markets/gold-rate-in-india-today',
-];
-export { url };
